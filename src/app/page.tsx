ced5a9d6e0458f9a5fdacd866dd04d9fb1e5f8ec
karan-palan <karanpalan007@gmail.com>
2025-03-14 13:07:37 +0530

    build: create project using bun and shadcn components

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button />
+      <p className="font-bold text-blue-500">Hi there</p>Hello world
+    </div>
+  );
+}
