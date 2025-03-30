98bdc4c21d2ae5c2a4a16c93bb06c64ee0959a2e
karan-palan <karanpalan007@gmail.com>
2025-03-30 16:01:27 +0530

    feat: personal section basic added and icon update

---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,55 @@
+"use client";
+import Link from "next/link";
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {HeartIcon, HistoryIcon, ListVideoIcon } from "lucide-react";
+
+const items = [
+  {
+    title: "History",
+    url: "/playlists/history",
+    icon: HistoryIcon,
+    auth: true,
+  },
+  {
+    title: "Liked Videos",
+    url: "/feed/liked",
+    icon: HeartIcon,
+  },
+  {
+    title: "Playlists",
+    url: "/feed/playlists",
+    icon: ListVideoIcon,
+  },
+];
+
+export const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} // change to look at pathname using router
+                // onClick={() => {}} // todo
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                    <item.icon/>
+                    <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
